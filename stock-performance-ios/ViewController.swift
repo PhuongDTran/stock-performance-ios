d04f86fa27993c4e0d652eef92ab3756bfbc4a1c
Phuong Tran <ptran816@uw.edu>
2020-07-27 02:27:54 -0700

    Initial Commit

---
+++ stock-performance-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stock-performance-ios
+//
+//  Created by Phuong Tran on 7/27/20.
+//  Copyright © 2020 Phuong Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
